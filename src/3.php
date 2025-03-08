af14adff591bd1b51858241dd1026b670c3f4fac
bywaterrauls586 <bywaterrauls586@gmail.com>
2025-03-08 22:23:44 +0000

    Add 3.php

---
+++ src/3.php
@@ -0,0 +1,19 @@
+function getRandomNumber() {
+    return rand(1, 100);
+}
+
+function getSum($num1, $num2) {
+    return $num1 + $num2;
+}
+
+function getDifference($num1, $num2) {
+    return $num1 - $num2;
+}
+
+function getProduct($num1, $num2) {
+    return $num1 * $num2;
+}
+
+function getQuotient($num1, $num2) {
+    return $num1 / $num2;
+}
